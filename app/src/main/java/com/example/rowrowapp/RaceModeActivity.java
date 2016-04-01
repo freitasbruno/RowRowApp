196d8412c2bcfdfd1f556ef76cdd4622f9c2b583
Bruno Freitas <freitascbruno@gmail.com>
2016-04-01 10:41:52 +0100

    Created the basic activities structure, including:
    - Events List (Used the scrollingActivity template).
    - Event Detail  (Used the scrollingActivity template).
    - Race Mode (for recording and regestering arrivals / departure)  (Used
    the blankActivity template).
    - An additional temporary activity, MainTestActivity with four buttons
    to allow to start each of the activities while the rest of the
    functionality is not yet implemented.
    
    Implemented the navigation functionality between the MainTestActivity to
    each of the other activities.

---
+++ app/src/main/java/com/example/rowrowapp/RaceModeActivity.java
@@ -0,0 +1,13 @@
+package com.example.rowrowapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RaceModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_race_mode);
+    }
+}
